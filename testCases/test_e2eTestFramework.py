50c79b955aec42616f957ad2fbe7f71758b28400
Shah Newaj <newajuiucse@gmail.com>
2025-06-03 10:10:21 +0600

    E2E test with pytest Framework

--- testCases/test_e2eTestFramework.py
+++ testCases/test_e2eTestFramework.py
@@ -1,24 +1,26 @@
+import json
 import os
 import sys
 import time
+import pytest
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.wait import WebDriverWait
 from pageObjects.loginPage import LoginPage
-from pageObjects.shopPage import ShopPage
 
 
-def test_e2e(browserInstance):
+test_data_path = './/testData/test_e2eTestFramework.json'
+with open(test_data_path) as f:
+    test_data = json.load(f)
+    test_list = test_data["data"]
+
+@pytest.mark.parametrize("test_list_item", test_list)
+def test_e2e(browserInstance, test_list_item):
     driver = browserInstance
-    driver.get("https://rahulshettyacademy.com/loginpagePractise/")
-    driver.maximize_window()
 
     loginPage = LoginPage(driver)
-    shopPage = loginPage.login()    #shopPage Object creation got encapsulated in loginPage.login()
-    shopPage.add_product_to_cart("Blackberry")
+    shopPage = loginPage.login(test_list_item["userEmail"], test_list_item["userPassword"])    #shopPage Object creation got encapsulated in loginPage.login()
+    shopPage.add_product_to_cart(test_list_item["productName"])
     # checkout_confirmation Object creation encapsulated in shopPage.goToCart()
     checkout_confirmation = shopPage.goToCart()
     checkout_confirmation.checkout()
